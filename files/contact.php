84c80dafedc8f58a12fda391e3b4c95d7f783053
JR-2004 <143631767+JR-2004@users.noreply.github.com>
2024-06-02 15:47:32 +0200

    Create contact.php

---
+++ files/contact.php
@@ -0,0 +1,20 @@
+<?php
+// Controleer of het formulier is ingediend
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verkrijg de ingediende gegevens van het formulier
+    $name = $_POST["name"];
+    $message = $_POST["message"];
+
+    // Verzend de e-mail
+    $to = "info.jr04@gmail.com";
+    $subject = "Contactformulier ingediend";
+    $body = "Naam: $name\n";
+    $body .= "E-mail: $email\n";
+    $body .= "Bericht:\n$message";
+    mail($to, $subject, $body);
+
+    // Redirect naar een bedankpagina
+    header("Location: bedankt.html");
+    exit();
+}
+?>
